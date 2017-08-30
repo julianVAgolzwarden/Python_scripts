3e04d981a922afddcbd0b1693bad636ae6f6145e
julianVAgolzwarden <golzwarden96@yahoo.de>
2017-08-30 21:57:58 +0200

    Looks good, try to make it faster (500.000 took 295s)

--- determinants3.py
+++ determinants3.py
@@ -7,7 +7,7 @@ t0 = time.time()
 with open('determinantData.txt') as f:
     content = f.readlines()
 
-n_ci = 1000 - 1
+n_ci = 500000 - 1
 norbs = 16
 nspinorbs = 2*norbs
 nelec = 12
@@ -21,23 +21,23 @@ for i in range(1, n_ci + 1):
     for j in range(nelec):
         dets[i-1, j] = int(content[i].split()[j + 2])
 
-ci.pop(0)
 ci_new = []
 for numbers in ci:
     ci_new.append(np.absolute(float(numbers)))
 
-occ = [6]*len(dets)
+pair = [6]*len(dets)
 for i in range(0, n_ci):
-    for j in range(nelec):
-        if dets[i, j+1] - dets[i, j] != 1:
-            
+    for j in range(nelec - 1):
+        for k in range(0 , 50, 2):
+            if dets[i, j] == k and dets[i, j + 1] == k + 1:
+                pair[i] = pair[i] - 1
+    pair[i] = pair[i]*2
 
-
-plt.title('ci coefficients vs excitation level')
-plt.xlabel('excitation level')
+plt.title('500000')
+plt.xlabel('unpaired electrons')
 plt.ylabel('ci coefficients')
-plt.plot(occ, ci_new, 'rs')
-plt.savefig('occupation_final2.png')
+plt.plot(pair, ci_new, 'rs')
+plt.savefig('unpaired.png')
 plt.show()
 
 print "Total Time:", time.time() - t0
