4fd9721d473ddb2baf80fe2d0c0f9c487e150945
julianVAgolzwarden <golzwarden96@yahoo.de>
2017-08-30 20:20:20 +0200

    First commit

---
+++ determinants.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+with open('determinantData.txt') as f:
+    data = f.readlines()
+
+ci = []
+Ed = []
+for line in data:
+    a = line.split()
+    ci.append(a[0])
+    Ed.append(a[1])
+
+ci.pop(0)
+Ed.pop(0)
+
+ci_new = []
+for numbers in ci:
+    ci_new.append(np.absolute(float(numbers)))
+
+Ed_new = []
+for items in Ed:
+    Ed_new.append(float(items))
+
+plt.title('ci Coefficients vs Determinant Energy')
+plt.xlabel('Determinant Energy')
+plt.ylabel('ci Coefficients')
+plt.plot(Ed_new, ci_new, 'bo')
+plt.savefig('energy_final')
+plt.show()
---
+++ determinants2.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+t0 = time.time()
+
+with open('determinantData.txt') as f:
+    content = f.readlines()
+
+n_ci = 1000 - 1
+norbs = 16
+nspinorbs = 2*norbs
+nelec = 12
+
+dets = np.zeros((n_ci, nelec))
+ci =[]
+
+for i in range(1, n_ci + 1):
+    a = content[i].split()
+    ci.append(a[0])
+    for j in range(nelec):
+        dets[i-1, j] = int(content[i].split()[j + 2])
+
+ci.pop(0)
+ci_new = []
+for numbers in ci:
+    ci_new.append(np.absolute(float(numbers)))
+
+hf_det = dets[0]
+print len(dets)
+exit(0)
+occ = []
+for i in range(1, n_ci):
+    mask = np.in1d(hf_det, dets[i])
+    occ.append(nelec - np.sum(mask))
+
+plt.title('ci coefficients vs excitation level')
+plt.xlabel('excitation level')
+plt.ylabel('ci coefficients')
+plt.plot(occ, ci_new, 'rs')
+plt.savefig('occupation_final2.png')
+plt.show()
+
+print "Total Time:", time.time() - t0
