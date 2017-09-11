f9bbb9edd42618e5a54f53ddae37f26395eaf8ba
julianVAgolzwarden <golzwarden96@yahoo.de>
2017-09-12 00:54:25 +0200

    The hamiltonianData had to be fixed

--- Save_Texts.py
+++ Save_Texts.py
@@ -18,13 +18,5 @@ for i in range(1, n_ci + 1):
     for j in range(nelec):
         dets[i-1, j] = int(content[i].split()[j + 2])
 
-pair = np.full(n_ci, 6)
-for i in range(0, n_ci):
-    for j in range(nelec - 1):
-        for k in range(0 , 31, 2):
-            if dets[i, j] == k and dets[i, j + 1] == k + 1:
-                pair[i] = pair[i] - 1
-    pair[i] = pair[i]*2
-
-np.savetxt('unpaired_electrons.txt', pair, fmt='%1i')
+np.savetxt('hamilton_matrix.txt', dets, fmt='%1i')
 print "Total Time:", time.time() - t0
